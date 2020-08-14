de8cc5ceb30a5d8ef6738a7859940637b52a3c06
Gabriel Nogueira <gabrieljcf@hotmail.com>
2020-08-14 20:37:18 -0300

    ajustando funcionalidade beber agua

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,25 +4,30 @@ class Main {
     this.inputNameEl = '';
     this.buttonEl = '';
 
-    this.dataUser = JSON.parse(localStorage.getItem('data_user'));
+    this.userData = JSON.parse(localStorage.getItem('data_user'));
     this.containerEl = document.querySelector('#app');
 
     this.getDataFromStorage();
   }
 
   getDataFromStorage() {
-    if (!this.dataUser) {
+    if (!this.userData) {
       this.renderInputsFromUser();
       this.calculateNeededWater();
     } else {
       this.renderNeededWaterFromUser();
     }
-
   }
 
   renderInputsFromUser() {
+    this.containerEl.innerHTML = '';
+
     const inputName = document.createElement('input');
+    inputName.setAttribute('placeholder', 'Informe seu Nome');
+
     const inputWeight = document.createElement('input');
+    inputWeight.setAttribute('placeholder', 'Informe seu Peso');
+
     const buttonEl = document.createElement('button');
     const textButton = document.createTextNode('Calcular');
 
@@ -39,7 +44,7 @@ class Main {
 
   renderNeededWaterFromUser() {
     this.containerEl.innerHTML = '';
-    
+
     if (this.inputWeightEl.value && this.inputNameEl.value) {
       this.inputWeightEl.remove();
       this.inputNameEl.remove();
@@ -47,9 +52,11 @@ class Main {
     }
 
     const title = document.createElement('h1');
-    const textOnTitle = document.createTextNode(`Você precisa de ${this.dataUser.waterNeeded} ml por dia`);
+    const textOnTitle = document.createTextNode(`${this.userData.name} Você precisa de ${this.userData.waterNeeded} ml`);
 
     const inputEl = document.createElement('input');
+    inputEl.setAttribute('placeholder', 'Informe quantas ml de água você bebeu');
+
     const buttonEl = document.createElement('button');
     const textButton = document.createTextNode('Beber água');
 
@@ -60,12 +67,18 @@ class Main {
     this.containerEl.appendChild(inputEl);
     this.containerEl.appendChild(buttonEl);
 
-    buttonEl.onclick = () => this.calculate(inputEl.value);
+    buttonEl.onclick = () => this.calculate(inputEl.value, title);
   }
 
   calculate(water = 0) {
-    const waterNeeded = this.dataUser.waterNeeded - water;
-    this.dataUser.waterNeeded = waterNeeded;
+    const waterNeeded = this.userData.waterNeeded - water;
+
+    this.userData.waterNeeded = waterNeeded;
+
+    if (waterNeeded <= 0) {
+      this.userData.waterNeeded = 0;
+    }
+
 
     this.saveToStorage();
     this.renderNeededWaterFromUser();
@@ -82,7 +95,7 @@ class Main {
         const weight = this.inputWeightEl.value;
         const waterNeeded = 35 * weight;
 
-        this.dataUser = {
+        this.userData = {
           name: this.inputNameEl.value,
           waterNeeded
         }
@@ -95,7 +108,7 @@ class Main {
   }
 
   saveToStorage() {
-    localStorage.setItem('data_user', JSON.stringify(this.dataUser));
+    localStorage.setItem('data_user', JSON.stringify(this.userData));
   }
 }
 
